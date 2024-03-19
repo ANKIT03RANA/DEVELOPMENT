0312e3a00bf8322c7f9177852dd60155db8144b5
ANKIT03RANA <ankitranaji3@gmail.com>
2024-03-19 10:58:26 +0530

    added images

--- MERN/E-Commerce/server/seeder.js
+++ MERN/E-Commerce/server/seeder.js
@@ -29,7 +29,7 @@ const importData = async () => {
     console.log('Data Imported!'.green.inverse);
     process.exit();
   } catch (error) {
-    console.error(`${error}`.red.inverse);
+    // console.error(`${error}`.red.inverse);
     process.exit(1);
   }
 };
@@ -43,7 +43,7 @@ const destroyData = async () => {
     console.log('Data Destroyed!'.red.inverse);
     process.exit();
   } catch (error) {
-    console.error(`${error}`.red.inverse);
+    // console.error(`${error}`.red.inverse);
     process.exit(1);
   }
 };
